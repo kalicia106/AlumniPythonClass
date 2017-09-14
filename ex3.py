0821144ba42feac29d62222e4812bd9ee9610b39
kalicia Nesbeth <kalicianesbeth@kalicia-Nesbeths-MacBook-Pro.local>
2017-09-14 15:22:40 -0400

    Here is Kalicia Nesbeth's exercise 3 homework

---
+++ ex3.py
@@ -0,0 +1,41 @@
+#A statement of Intent
+print("I will now count my chickens:")
+
+#An equation to give back the count of Hens
+print("Hens", 25 + 30 / 6)
+
+#An equation to give back the count of Roosters
+print("Roosters", 100 - 25 * 3 % 4)
+
+#A statement of Intent
+print("Now I will count the eggs:")
+
+#Perform an opertion using PEMDA to find the count of eggs
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+
+#A string with a boolean logic expression
+print("Is it true that 3 + 2 < 5 - 7?")
+
+#Boolean logic expression
+print(3 + 2 < 5 - 7)
+
+#A mathematical question with the equation's answer
+print("What is 3 + 2?", 3 + 2)
+
+#A mathehematical question with the equation's answer
+print("What is 5 - 7?", 5 - 7)
+
+#Response to question the previous questions answer
+print("Oh, that's why it's False.")
+
+#statement of intent
+print("How about some more.")
+
+#String wth Boolean logic expression
+print("Is it greater?", 5 > -2)
+
+#String with Boolean logic expression
+print("Is it greater or equal?", 5 >= -2)
+
+#String with Boolean logic expression
+print("Is it less or equal?", 5 <= -2)
