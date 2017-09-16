31db0df049e1504ae2189c9547df85bdbc7cb86b
kalicia Nesbeth <kalicianesbeth@kalicia-Nesbeths-MacBook-Pro.local>
2017-09-16 11:38:00 -0400

    Instructor on class exercise.Kalicia Nesbeth

---
+++ ex19a.py
@@ -0,0 +1,20 @@
+#write the function that sums 3 numbers
+#call the function with the following numbers
+
+#19, -8, 9
+#15, 9, 8
+#30, -29, 5
+
+#define a function
+def Addingthreenumbers(x, y, z):
+
+#name the variable for the equation   
+  results = (x + y + z)
+
+ #pint the answers
+  print(results)
+
+#call the function with defined variables
+Addingthreenumbers(19, -8, 9)
+Addingthreenumbers(15, 9, 8)
+Addingthreenumbers(30, -29, 5)
