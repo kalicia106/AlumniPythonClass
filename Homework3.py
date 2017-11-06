190cda101ed2fe8adf87f15dba42f38e18a31336
kalicia Nesbeth <kalicianesbeth@kalicia-Nesbeths-MacBook-Pro.local>
2017-11-06 02:04:38 -0500

    Kalicia Nesbeth Part 1 of 3 for Homework 3

---
+++ Homework3.py
@@ -0,0 +1,20 @@
+#Homework #3
+
+#Exercise 15
+#Reading Files
+
+from sys import argv
+
+script, filename = argv
+
+txt = open(filename)
+
+print(f"Here's your file {filename}")
+print(txt.read())
+
+print("Type the file name again:")
+file_again = input(">")
+
+txt_again = open(file_again)
+
+print(txt_again.read())
