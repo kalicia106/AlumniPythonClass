bc75d6fbd648e4db0ef845f0bcd5ae244c890fe8
kalicia Nesbeth <kalicianesbeth@kalicia-Nesbeths-MacBook-Pro.local>
2017-11-07 19:57:51 -0500

    Kalicia Nesbeth 1 of 2 for Homework# 4

---
+++ HW4.py
@@ -0,0 +1,7 @@
+#Homework 4
+
+#Google how to convert strings from lower to uppercase and upper to lowercase.
+#Show your work(screenshot) and add the resources you used.
+
+string = 'Kalicia Nesbeth'
+print(string.upper())
